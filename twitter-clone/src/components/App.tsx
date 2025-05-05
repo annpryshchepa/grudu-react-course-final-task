9ac9c7a902772e7be735911a5e1085da4218836a
hpryshchepa <hpryshchepa@griddynamics.com>
2025-05-05 17:51:44 +0300

    lint fix

--- twitter-clone/src/components/App.tsx
+++ twitter-clone/src/components/App.tsx
@@ -1,17 +1,20 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './AuthProvider';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./AuthProvider";
 
 function App() {
   return (
     <Router>
-    <AuthProvider>
-      <Routes>
-      <Route path="/login" element={<div className='bg-red'>login</div>} />
-        <Route path="/singup" element={<div className='bg-red'>signup</div>} />
+      <AuthProvider>
+        <Routes>
+          <Route path="/login" element={<div className="bg-red">login</div>} />
+          <Route
+            path="/singup"
+            element={<div className="bg-red">signup</div>}
+          />
           <Route path="/" element={<div>home</div>} />
           <Route path="*" element={<div>not found</div>} />
-          </Routes>
-          </AuthProvider>
+        </Routes>
+      </AuthProvider>
     </Router>
   );
 }
--- twitter-clone/src/components/AuthProvider.tsx
+++ twitter-clone/src/components/AuthProvider.tsx
@@ -3,25 +3,21 @@ import { createContext, useContext, useState } from "react";
 type UserType = {
   name: string;
   email: string;
-}
+};
 
-const defaultUser = { name: "", email: "" }
+const defaultUser = { name: "", email: "" };
 
 const AuthContext = createContext(defaultUser);
-export const AuthProvider = ({ children }: {children: React.ReactNode}) => {
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const [user, setUser] = useState<UserType>(defaultUser);
-  const login = (newUser:UserType) => {
+  const login = (newUser: UserType) => {
     setUser(newUser);
   };
   const logout = () => {
     setUser(defaultUser);
   };
 
-  return (
-    <AuthContext.Provider value={user}>
-      {children}
-    </AuthContext.Provider>
-  );
+  return <AuthContext.Provider value={user}>{children}</AuthContext.Provider>;
 };
 export const useAuth = () => {
   const context = useContext(AuthContext);
